8e850f77c5dc186f20ec44dfe502a157b8f9d763
Welman P <welman91ers@gmail.com>
2020-08-22 18:24:51 +1000

    commit awal

---
+++ assets/js/myscript.js
@@ -0,0 +1,71 @@
+    // top sale owl carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    // new phones owl carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    // new accessories owl carousel
+    $("#new-accessories .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    // new electronics owl carousel
+    $("#new-electronics .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
